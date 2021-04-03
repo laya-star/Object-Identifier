fbdc39c4a30d75bda14db465bbb06d2c7d7d44aa
laya-star <78084935+laya-star@users.noreply.github.com>
2021-04-03 14:41:36 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,22 @@
+status="";
+input="";
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    input=document.getElementById("object").value;
+
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+}
+function draw(){
+    image(video,0,0,480,380);
+}
